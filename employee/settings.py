694d42c600fea51eb0d4ec1b7d745ddb264a35b6
DESKTOP-H5540Q0\aljay <aljayfauni@gmail.com>
2023-10-13 16:51:53 +0800

    first commit

---
+++ employee/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'django_crud',  
+        'USER':'root',  
+        'PASSWORD':'',  
+        'HOST':'localhost',  
+        'PORT':'3307'  
+    }  
+}  
